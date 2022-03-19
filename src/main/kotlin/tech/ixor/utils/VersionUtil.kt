5393b6749ab0bb901eb0e995fff3c796513a217a
Cubik65536 <cubik65536@cubik65536.top>
2022-03-18 23:42:02 -0400

    fix: Access to version.properties in jar
    Use getResourceAsStream for getting version properties, so it can be
    accessed in jar & dev env.

--- src/main/kotlin/tech/ixor/utils/VersionUtil.kt
+++ src/main/kotlin/tech/ixor/utils/VersionUtil.kt
@@ -1,22 +1,14 @@
 package tech.ixor.utils
-
-import java.io.FileInputStream
-import java.io.FileNotFoundException
+import java.io.InputStream
 import java.util.*
+
 
 class VersionUtil {
     companion object {
         fun getVersion(): String {
-            val pwd = System.getProperty("user.dir")
-            var fileInputStream: FileInputStream
-            try {
-                fileInputStream = FileInputStream("$pwd/version.properties")
-            } catch (e: FileNotFoundException) {
-                println("Fallback to version properties in development environment")
-                fileInputStream = FileInputStream("$pwd/src/main/resources/version.properties")
-            }
+            var inputStream: InputStream = this::class.java.getResourceAsStream("/version.properties")
             val properties = Properties()
-            properties.load(fileInputStream)
+            properties.load(inputStream)
             return "Version: ${properties.getProperty("version")}-${properties.getProperty("stage")}" +
                     " (${properties.getProperty("revision").uppercase()})"
         }
