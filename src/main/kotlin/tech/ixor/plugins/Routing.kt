70bcf90cafde3fa2ecd9e3604e7d88710d595014
Cubik65536 <cubik65536@cubik65536.top>
2022-03-20 21:17:54 -0400

    feat: Add index routing page after installation

--- src/main/kotlin/tech/ixor/plugins/Routing.kt
+++ src/main/kotlin/tech/ixor/plugins/Routing.kt
@@ -1,16 +1,14 @@
 package tech.ixor.plugins
 
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.html.*
+import tech.ixor.utils.VersionUtil
 
 fun Application.configureRouting() {
-
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondHtml(block = Index(VersionUtil.getVersion()).index)
         }
     }
 }
