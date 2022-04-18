afad99456f0fdb5d6ac3651e737b838570b60871
Cubik65536 <cubik65536@cubik65536.top>
2022-04-18 16:08:07 -0400

    feat: Add API for execute MC Server command

--- src/main/kotlin/tech/ixor/routes/controller/mcserver/MCServerController.kt
+++ src/main/kotlin/tech/ixor/routes/controller/mcserver/MCServerController.kt
@@ -1,11 +1,50 @@
 package tech.ixor.routes.controller.mcserver
 
 import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
 import io.ktor.routing.*
+import tech.ixor.entity.ConfigEntity
+import tech.ixor.entity.MinecraftServerEntity
+import tech.ixor.entity.MinecraftServers
+
+fun Route.mcServerExecuteCommand() {
+    val authKey = ConfigEntity().loadConfig().authKey
+
+    post("/mcserver/execute_command") {
+        val request = call.receive<MCServerExecuteCommandRequest>()
+        if (authKey != request.authKey) {
+            call.respondText("Auth key (on your client) is not valid", status = HttpStatusCode.Forbidden)
+            return@post
+        }
+
+        val host = request.host
+        val port = request.port
+        val minecraftServer: MinecraftServerEntity? = MinecraftServers.getServer(host, port)
+        if (minecraftServer != null) {
+            val command = request.command
+            val mcServerResponse = minecraftServer.executeCommand(command)
+            if (mcServerResponse.statusCode == 200) {
+                call.respondText("Message sent!", status = HttpStatusCode.OK)
+            } else if (mcServerResponse.statusCode == 401) {
+                call.respondText("Auth key (on RemoteMC-Core) is not valid", status = HttpStatusCode.Forbidden)
+            } else if (mcServerResponse.statusCode == 500 && mcServerResponse.message == "Server is offline") {
+                call.respondText("Target server offline", status = HttpStatusCode.InternalServerError)
+            } else {
+                call.respondText("Unknown error", status = HttpStatusCode.InternalServerError)
+            }
+        } else {
+            call.respondText("Server not found", status = HttpStatusCode.NotFound)
+            return@post
+        }
+    }
+}
 
 fun Application.registerMCServerController() {
     routing {
         mcServerStatus()
         mcServerMessaging()
+        mcServerExecuteCommand()
     }
 }
--- src/main/kotlin/tech/ixor/routes/controller/mcserver/MCServerRequests.kt
+++ src/main/kotlin/tech/ixor/routes/controller/mcserver/MCServerRequests.kt
@@ -1,5 +1,10 @@
 package tech.ixor.routes.controller.mcserver
 
+data class MCServerExecuteCommandRequest(
+    val host: String, val port: Int,
+    val authKey: String, val command: String
+)
+
 data class MCServerStatusRequest(
     val host: String, val port: Int
 )
