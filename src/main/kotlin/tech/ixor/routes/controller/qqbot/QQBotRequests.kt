0680f9529eb750902cec81a752ed934d5bf20f82
Cubik65536 <cubik65536@cubik65536.top>
2022-05-10 21:44:13 -0400

    fix: Ask for authKey while sending qqbot messaging request

--- src/main/kotlin/tech/ixor/routes/controller/qqbot/QQBotRequests.kt
+++ src/main/kotlin/tech/ixor/routes/controller/qqbot/QQBotRequests.kt
@@ -1,7 +1,7 @@
 package tech.ixor.routes.controller.qqbot
 
 data class QQBotSendMessageRequest(
-    val host: String, val port: Int,
+    val host: String, val port: Int, val authKey: String,
     val groupCode: Int, val source: String,
     val sender: String, val message: String
 )
