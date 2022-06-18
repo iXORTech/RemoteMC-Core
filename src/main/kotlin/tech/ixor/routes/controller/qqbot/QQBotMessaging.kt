e99b991f845e97a1efa49d7295f18e5d10ea3a3f
Cubik65536 <cubik65536@cubik65536.top>
2022-06-18 16:53:09 -0400

    feat(config): Add Group Code check before send message

--- src/main/kotlin/tech/ixor/routes/controller/qqbot/QQBotMessaging.kt
+++ src/main/kotlin/tech/ixor/routes/controller/qqbot/QQBotMessaging.kt
@@ -8,6 +8,7 @@ import io.ktor.server.routing.*
 import tech.ixor.entity.ConfigEntity
 import tech.ixor.entity.QQBotEntity
 import tech.ixor.entity.QQBot
+import tech.ixor.entity.QQGroups
 
 fun Route.qqBotMessaging() {
     val authKey = ConfigEntity().loadConfig().authKey
@@ -30,12 +31,22 @@ fun Route.qqBotMessaging() {
             )
             return@post
         }
+
+        val qqGroup = QQGroups.getQQGroup(groupCode)
+        if (qqGroup == null) {
+            call.respondText(
+                "The group code ${groupCode} is not valid! Check your RemoteMC-Core config file!",
+                status = HttpStatusCode.NotFound
+            )
+            return@post
+        }
+
         val source = request.source
         val sender = request.sender
         val message = request.message
         val qqBotResponse = qqBot.sendMessage(groupCode, source, sender, message)
         when (qqBotResponse.statusCode) {
-            200 -> call.respondText("Message sent successfully!", status = HttpStatusCode.OK)
+            200 -> call.respondText("Message sent successfully to ${qqGroup.groupName}!", status = HttpStatusCode.OK)
             401 -> call.respondText(
                 "Auth key on RemoteMC-Core is not valid! Please check authKey settings and make sure" +
                         " they were the same everywhere!", status = HttpStatusCode.Unauthorized
