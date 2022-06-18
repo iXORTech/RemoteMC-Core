27267aeb914c109098a177669948b3830073fd8a
Cubik65536 <cubik65536@cubik65536.top>
2022-06-18 15:38:11 -0400

    feature(web): Apply style to status webpage

--- src/main/kotlin/tech/ixor/routes/web/Status.kt
+++ src/main/kotlin/tech/ixor/routes/web/Status.kt
@@ -17,100 +17,120 @@ fun Route.status() {
             minecraftServerAliveMonitor.forceUpdate()
             head {
                 title { +"${I18N.website_status}" }
+                meta { charset = "utf-8" }
+                link( rel = "stylesheet", href = "/assets/style.css", type = "text/css")
+                meta { name = "viewport"; content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" }
+                script { src = "https://unpkg.com/feather-icons" }
             }
             body {
-                h2 { +"${I18N.website_status}" }
+                div {
+                    id = "page-wrapper"
+                    div("index-container") {
+                        div("content") {
+                            h2 { +"${I18N.website_status}" }
 
-                h3 {
-                    +"${I18N.connections}"
-                }
+                            br { }
 
-                h4 {
-                    +"${I18N.minecraft_server}"
-                }
-                h5 {
-                    +"${I18N.online} "
-                }
-                ol {
-                    val onlineServers = MinecraftServers.getOnlineServers()
-                    for (minecraftServer in onlineServers) {
-                        li {
-                            b {
-                                +minecraftServer.serverName
-                            }
-                            +" - ${minecraftServer.host} : ${minecraftServer.port}"
-                            if (minecraftServer.default &&
-                                MinecraftServers.getDefaultServer()?.equals(minecraftServer) == true
-                            ) {
-                                i {
-                                    +" ${I18N.default}"
+                            h2 {
+                                b ("h1a") {
+                                    +"${I18N.connections}"
                                 }
                             }
-                        }
-                    }
-                }
-                h5 {
-                    +"${I18N.offline}"
-                }
-                ol {
-                    val offlineServers = MinecraftServers.getOfflineServers()
-                    for (minecraftServer in offlineServers) {
-                        li {
-                            b {
-                                +minecraftServer.serverName
+
+                            h3 {
+                                +"${I18N.minecraft_server}"
                             }
-                            +" - ${minecraftServer.host} : ${minecraftServer.port}"
-                        }
-                    }
-                }
-
-                h4 {
-                    +"${I18N.qq_chat_bots}"
-                }
-                h5 {
-                    +"${I18N.online} "
-                }
-                ol {
-                    val onlineBots = QQBots.getOnlineBots()
-                    for (qqBot in onlineBots) {
-                        li {
-                            b {
-                                +"${qqBot.host} : ${qqBot.port}"
+                            h4 {
+                                +"${I18N.online} "
                             }
-                            +" - ${qqBot.groupName} (${qqBot.groupCode})"
-                        }
-                    }
-                }
-                h5 {
-                    +"${I18N.offline}"
-                }
-                ol {
-                    val offlineBots = QQBots.getOfflineBots()
-                    for (qqBot in offlineBots) {
-                        li {
-                            b {
-                                +"${qqBot.host} : ${qqBot.port}"
+                            ol {
+                                val onlineServers = MinecraftServers.getOnlineServers()
+                                for (minecraftServer in onlineServers) {
+                                    li {
+                                        b {
+                                            +minecraftServer.serverName
+                                        }
+                                        +" - ${minecraftServer.host} : ${minecraftServer.port}"
+                                        if (minecraftServer.default &&
+                                            MinecraftServers.getDefaultServer()?.equals(minecraftServer) == true
+                                        ) {
+                                            i {
+                                                +" ${I18N.default}"
+                                            }
+                                        }
+                                    }
+                                }
                             }
-                            +" - ${qqBot.groupName} (${qqBot.groupCode})"
+                            h4 {
+                                +"${I18N.offline}"
+                            }
+                            ol {
+                                val offlineServers = MinecraftServers.getOfflineServers()
+                                for (minecraftServer in offlineServers) {
+                                    li {
+                                        b {
+                                            +minecraftServer.serverName
+                                        }
+                                        +" - ${minecraftServer.host} : ${minecraftServer.port}"
+                                    }
+                                }
+                            }
+
+                            h3 {
+                                +"${I18N.qq_chat_bots}"
+                            }
+                            h4 {
+                                +"${I18N.online} "
+                            }
+                            ol {
+                                val onlineBots = QQBots.getOnlineBots()
+                                for (qqBot in onlineBots) {
+                                    li {
+                                        b {
+                                            +"${qqBot.host} : ${qqBot.port}"
+                                        }
+                                        +" - ${qqBot.groupName} (${qqBot.groupCode})"
+                                    }
+                                }
+                            }
+                            h4 {
+                                +"${I18N.offline}"
+                            }
+                            ol {
+                                val offlineBots = QQBots.getOfflineBots()
+                                for (qqBot in offlineBots) {
+                                    li {
+                                        b {
+                                            +"${qqBot.host} : ${qqBot.port}"
+                                        }
+                                        +" - ${qqBot.groupName} (${qqBot.groupCode})"
+                                    }
+                                }
+                            }
+
+                            br {}
+
+                            footer {
+                                hr {}
+                                a (href = "https://github.com/iXORTech/RemoteMC-Core/issues") {
+                                    +"${I18N.reportBug}"
+                                }
+                                hr {}
+                                a (href = "https://github.com/iXORTech") {
+                                    i { attributes["data-feather"] = "github" }
+                                }
+                                +" | ${I18N.poweredBy} "
+                                a (href = "https://ixor.tech") { +"iXOR Technology" }
+                                +" ${I18N.withLove}"
+                                br {}
+                                + "${I18N.htmlThemeDesigned0}"
+                                a (href = "https://github.com/athul/archie") { +"Archie Theme" }
+                                + "${I18N.htmlThemeDesigned1}"
+                                a (href = "https://github.com/KevinZonda") { +"@KevinZonda" }
+                                + "${I18N.htmlThemeDesigned2}"
+                            }
                         }
                     }
-                }
-
-                br {}
-                hr {}
-
-                p {
-                    a(href = "https://github.com/iXORTech/RemoteMC-Core/issues") {
-                        +"${I18N.reportBug}"
-                    }
-                }
-
-                hr {}
-
-                p {
-                    +"${I18N.poweredBy} "
-                    a(href = "https://ixor.tech") { +"iXOR Technology" }
-                    +" ${I18N.withLove}"
                 }
             }
         }
