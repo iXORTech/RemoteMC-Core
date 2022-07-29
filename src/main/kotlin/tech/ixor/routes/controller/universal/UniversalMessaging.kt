7bf4b86a803eeed67a6d2041d3a0606d75ae0fbd
Cubik65536 <cubik65536@cubik65536.top>
2022-07-29 13:55:47 -0400

    chore(routes/controller/universal): Add TODO comment for Universal Messaging Routes

--- src/main/kotlin/tech/ixor/routes/controller/universal/UniversalMessaging.kt
+++ src/main/kotlin/tech/ixor/routes/controller/universal/UniversalMessaging.kt
@@ -1,7 +1,18 @@
 package tech.ixor.routes.controller.universal
 
 import io.ktor.server.routing.*
+import tech.ixor.entity.ConfigEntity
 
 fun Route.universalMessaging() {
+    val authKey = ConfigEntity().loadConfig().authKey
 
+    post("/send-message") {
+        // TODO：Add messaging route that send message to all clients (Minecraft Servers, QQ ChatBots, etc.)
+
+    }
+
+    post("/broadcast") {
+        // TODO：Add broadcast route that broadcast message to all clients (Minecraft Servers, QQ ChatBots, etc.)
+
+    }
 }
