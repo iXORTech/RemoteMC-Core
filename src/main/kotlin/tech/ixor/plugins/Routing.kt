aedf2f8066debbb6a20cd358df16254bd568916f
Cubik65536 <cubik65536@cubik65536.top>
2022-07-29 13:34:30 -0400

    refactor: Remove Messaging routing for QQBots
    BREAKING-CHANGE: Remove Messaging routing (web APIs) for QQ Bots. Replacing it with a universal (send to MC Servers + QQBots + Other Chat Bots) solution.

--- src/main/kotlin/tech/ixor/plugins/Routing.kt
+++ src/main/kotlin/tech/ixor/plugins/Routing.kt
@@ -2,11 +2,9 @@ package tech.ixor.plugins
 
 import io.ktor.server.application.*
 import tech.ixor.routes.controller.mcserver.registerMCServerRoutes
-import tech.ixor.routes.controller.qqbot.registerQQBotRoutes
 import tech.ixor.routes.web.registerWebRoutes
 
 fun Application.configureRouting() {
     registerWebRoutes()
     registerMCServerRoutes()
-    registerQQBotRoutes()
 }
--- src/main/kotlin/tech/ixor/routes/controller/qqbot/QQBotRequests.kt
+++
@@ -1,6 +0,0 @@
-package tech.ixor.routes.controller.qqbot
-
-data class QQBotSendMessageRequest(
-    val authKey: String, val groupCode: Long,
-    val source: String, val sender: String, val message: String
-)
