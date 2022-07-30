67fde99c1d3f46ba63b27f3090c697febd729c1f
Cubik65536 <cubik65536@cubik65536.top>
2022-07-30 14:45:58 -0400

    feat(job/AliveMonitor): Log information when update online status

--- src/main/kotlin/tech/ixor/job/MinecraftServerAliveMonitor.kt
+++ src/main/kotlin/tech/ixor/job/MinecraftServerAliveMonitor.kt
@@ -4,15 +4,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import dev.inmo.krontab.doInfinity
+import org.slf4j.LoggerFactory
 
 import tech.ixor.entity.MinecraftServers
 
 class MinecraftServerAliveMonitor() {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     fun start() {
         forceUpdate()
         val servers = MinecraftServers.getAllServers()
         CoroutineScope(Dispatchers.Default).launch {
             doInfinity("0 /5 * * *") {
+                logger.info("[Scheduled Task] Updating Minecraft Server online status")
                 servers.forEach {
                     it.updateOnlineStatus()
                 }
@@ -22,6 +26,7 @@ class MinecraftServerAliveMonitor() {
 
     fun forceUpdate() {
         val servers = MinecraftServers.getAllServers()
+        logger.info("[Force Trigger] Updating Minecraft Server online status")
         servers.forEach {
             it.updateOnlineStatus()
         }
--- src/main/kotlin/tech/ixor/job/QQBotAliveMonitor.kt
+++ src/main/kotlin/tech/ixor/job/QQBotAliveMonitor.kt
@@ -4,14 +4,18 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import dev.inmo.krontab.doInfinity
+import org.slf4j.LoggerFactory
 import tech.ixor.entity.QQBot
 
 class QQBotAliveMonitor {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     fun start() {
         forceUpdate()
         val bot = QQBot.getBot()
         CoroutineScope(Dispatchers.Default).launch {
             doInfinity("0 /5 * * *") {
+                logger.info("[Scheduled Task] Updating QQ Bot online status")
                 bot.updateOnlineStatus()
             }
         }
@@ -19,6 +23,7 @@ class QQBotAliveMonitor {
 
     fun forceUpdate() {
         val bot = QQBot.getBot()
+        logger.info("[Force Trigger] Updating QQ Bot online status")
         bot.updateOnlineStatus()
     }
 
