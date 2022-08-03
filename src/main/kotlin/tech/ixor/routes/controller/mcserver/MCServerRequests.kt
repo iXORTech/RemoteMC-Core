74264280f488bcf975f044e01ac4c19d155dfeb9
Cubik65536 <cubik65536@cubik65536.top>
2022-08-03 16:40:26 -0400

    chore(routes/controller/mcserver): Remove unused HTTP Requests

--- src/main/kotlin/tech/ixor/routes/controller/mcserver/MCServerRequests.kt
+++ src/main/kotlin/tech/ixor/routes/controller/mcserver/MCServerRequests.kt
@@ -8,12 +8,3 @@ data class MCServerExecuteCommandRequest(
 data class MCServerStatusRequest(
     val host: String?, val port: Int?
 )
-
-data class MCServerSayRequest(
-    val host: String?, val port: Int?, val authKey: String?,
-    val source: String?, val sender: String?, val message: String?
-)
-
-data class MCServerBroadcastRequest(
-    val host: String?, val port: Int?, val authKey: String?, val message: String?
-)
