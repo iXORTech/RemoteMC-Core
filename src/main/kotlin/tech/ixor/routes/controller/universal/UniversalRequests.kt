c1ce76c28d0fc6a80d43880bfa832299f02804ad
Cubik65536 <cubik65536@cubik65536.top>
2022-08-05 12:20:21 -0400

    refactor(routes/controller/universal): Change `messageID` in UniversalSendMessageRequest to `senderID`

--- src/main/kotlin/tech/ixor/routes/controller/universal/UniversalRequests.kt
+++ src/main/kotlin/tech/ixor/routes/controller/universal/UniversalRequests.kt
@@ -1,7 +1,7 @@
 package tech.ixor.routes.controller.universal
 
 data class UniversalSendMessageRequest(
-    val authKey: String?, val messageID: String?,
+    val authKey: String?, val senderID: String?,
     val source: String?, val sender: String?, val message: String?
 )
 
