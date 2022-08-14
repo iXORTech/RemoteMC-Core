da0a1b2a91a75c75dfa3a65a4569be2ddce7d67a
Cubik65536 <cubik65536@cubik65536.top>
2022-08-13 20:26:49 -0400

    build: Save built jar to `build/distributions`

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ tasks {
         if (getStage() == "dev" || getStage() == "alpha" || getStage() == "beta" || getStage() == "rc") {
             shadowJarVersion = shadowJarVersion + "+" + getRevision()
         }
+        destinationDirectory.set(file("${projectDir}/build/distributions"))
         archiveVersion.set(shadowJarVersion)
         archiveClassifier.set("")
     }
