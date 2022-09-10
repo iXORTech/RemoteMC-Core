721f2daa46fb9f1200f08c00511d1dc73a858ddc
Cubik65536 <cubik65536@cubik65536.top>
2022-09-10 16:52:53 -0400

    refactor(minecraftserverentity): Improve 503 Response

--- src/main/kotlin/tech/ixor/entity/HTTPResponse.kt
+++ src/main/kotlin/tech/ixor/entity/HTTPResponse.kt
@@ -1,6 +1,8 @@
 package tech.ixor.entity
 
 import com.beust.klaxon.Json
+import org.slf4j.Logger
+import tech.ixor.I18N
 
 class HTTPResponse(
     @Json(name = "status_code")
@@ -9,5 +11,14 @@ class HTTPResponse(
 ) {
     override fun toString(): String {
         return "HTTPResponse(statusCode=$statusCode, message='$message')"
+    }
+
+    companion object {
+        fun get503(logger: Logger, serverName: String): HTTPResponse {
+            val response = HTTPResponse(503, "Service Unavailable")
+            logger.info(I18N.logging_mcServerOffline(serverName))
+            logger.info(I18N.logging_sendingResponse(response.toString()))
+            return response
+        }
     }
 }
