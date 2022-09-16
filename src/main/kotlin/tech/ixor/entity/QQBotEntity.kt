bdca32b469ef137805abdd6b73e1ff3978f81ca6
Cubik65536 <cubik65536@cubik65536.top>
2022-09-16 13:10:22 -0400

    feat(qqgroupentity): Add log messages

--- src/main/kotlin/tech/ixor/entity/QQBotEntity.kt
+++ src/main/kotlin/tech/ixor/entity/QQBotEntity.kt
@@ -6,18 +6,26 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import org.slf4j.LoggerFactory
 import tech.ixor.I18N
 import java.net.ConnectException
 
 class QQBotEntity constructor(
     host: String, port: Int, ssl: Boolean
 ) : ServerEntity(I18N.qqBotServerName(), host, port, ssl) {
+    private var logger = LoggerFactory.getLogger(javaClass)
+
     inner class QQGroupEntity constructor(
         val groupName: String, val groupCode: Long
     ) {
         private val authKey = ConfigEntity().loadConfig().authKey
 
+        override fun toString(): String {
+            return "QQGroupEntity(groupName='$groupName', groupCode=$groupCode)"
+        }
+
         private suspend fun sendRequest(url: String): HTTPResponse {
+            logger.info(I18N.logging_sendingRequestToUrl(url))
             val client = HttpClient(CIO)
             val response = Klaxon().parse<HTTPResponse>(
                 try {
@@ -26,15 +34,24 @@ class QQBotEntity constructor(
                     }
                         .body<String>().toString()
                 } catch (e: ConnectException) {
-                    return HTTPResponse(statusCode = 503, message = "SERVICE_UNAVAILABLE")
+                    return HTTPResponse.get503(logger, serverName)
                 }
             )
-            return response ?: HTTPResponse(statusCode = 503, message = "SERVICE_UNAVAILABLE")
+
+            return if (response != null) {
+                logger.info(I18N.logging_responseFromUrl(url, response.toString()))
+                logger.info(I18N.logging_sendingResponse(response.toString()))
+                response
+            } else {
+                HTTPResponse.get503(logger, serverName)
+            }
         }
 
         suspend fun sendMessage(senderID: String, source: String, sender: String, message: String): HTTPResponse {
+            logger.info(I18N.logging_qqGroupSendMessageRequestReceived(groupName, groupCode))
+            logger.info(I18N.logging_qqGroupSendMessageRequestParams(senderID, source, sender, message))
             if (!checkOnlineStatus()) {
-                return HTTPResponse(statusCode = 503, message = "SERVICE_UNAVAILABLE")
+                return HTTPResponse.get503(logger, serverName)
             }
             val url = getUrl() +
                     "/groupMessage?authKey=$authKey&group=$groupCode&sender_id=$senderID&source=$source&sender=$sender&message=$message"
@@ -42,8 +59,9 @@ class QQBotEntity constructor(
         }
 
         suspend fun broadcast(message: String): HTTPResponse {
+            logger.info(I18N.logging_qqGroupBroadcastRequestReceived(groupName, groupCode, message))
             if (!checkOnlineStatus()) {
-                return HTTPResponse(statusCode = 503, message = "SERVICE_UNAVAILABLE")
+                return HTTPResponse.get503(logger, serverName)
             }
             val url = getUrl() +
                     "/groupBroadcast?authKey=$authKey&group=$groupCode&message=$message"
@@ -54,42 +72,68 @@ class QQBotEntity constructor(
     private val qqGroups = mutableListOf<QQGroupEntity>()
 
     fun addQQGroup(qqGroup: QQGroupEntity) {
+        logger.info(I18N.logging_addingQQGroup(qqGroup.toString()))
         qqGroups.add(qqGroup)
     }
 
     fun addQQGroup(qqGroup: ConfigEntity.QQGroupConfig) {
-        qqGroups.add(QQGroupEntity(qqGroup.groupName, qqGroup.groupCode))
+        val group = QQGroupEntity(qqGroup.groupName, qqGroup.groupCode)
+        logger.info(I18N.logging_addingQQGroup(group.toString()))
+        qqGroups.add(group)
     }
 
     fun addQQGroup(groupName: String, groupCode: Long) {
-        qqGroups.add(QQGroupEntity(groupName, groupCode))
+        val group = QQGroupEntity(groupName, groupCode)
+        logger.info(I18N.logging_addingQQGroup(group.toString()))
+        qqGroups.add(group)
     }
 
     fun getQQGroups(): List<QQGroupEntity> {
+        logger.info(I18N.logging_gettingAllQQGroups())
         return qqGroups
     }
 
     fun getQQGroup(groupCode: Long): QQGroupEntity? {
+        logger.info(I18N.logging_gettingQQGroupAccordingToCode(groupCode))
         return qqGroups.find { it.groupCode == groupCode }
     }
 }
 
 object QQBot {
     private lateinit var qqBot: QQBotEntity
+    private var logger = LoggerFactory.getLogger(javaClass)
 
     fun setBot(bot: QQBotEntity) {
+        if (bot.ssl) {
+            logger.info(I18N.logging_settingQQBot(bot.host, bot.port, I18N.enabled()))
+        } else {
+            logger.info(I18N.logging_settingQQBot(bot.host, bot.port, I18N.disabled()))
+        }
         qqBot = bot
     }
 
-    fun setBot(bot: ConfigEntity.QQBotConfig) {
-        qqBot = QQBotEntity(bot.host, bot.port, bot.ssl)
+    fun setBot(botConfig: ConfigEntity.QQBotConfig) {
+        val bot = QQBotEntity(botConfig.host, botConfig.port, botConfig.ssl)
+        if (bot.ssl) {
+            logger.info(I18N.logging_settingQQBot(bot.host, bot.port, I18N.enabled()))
+        } else {
+            logger.info(I18N.logging_settingQQBot(bot.host, bot.port, I18N.disabled()))
+        }
+        qqBot = bot
     }
 
     fun setBot(host: String, port: Int, ssl: Boolean) {
-        qqBot = QQBotEntity(host, port, ssl)
+        val bot = QQBotEntity(host, port, ssl)
+        if (bot.ssl) {
+            logger.info(I18N.logging_settingQQBot(bot.host, bot.port, I18N.enabled()))
+        } else {
+            logger.info(I18N.logging_settingQQBot(bot.host, bot.port, I18N.disabled()))
+        }
+        qqBot = bot
     }
 
     fun getBot(): QQBotEntity {
+        logger.info(I18N.logging_gettingQQBot())
         return qqBot
     }
 }
