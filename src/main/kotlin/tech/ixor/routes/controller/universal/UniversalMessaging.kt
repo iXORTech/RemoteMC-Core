957940244725483596cfbc9f73db38c389b6e827
Cubik65536 <cubik65536@cubik65536.top>
2022-09-20 23:43:37 -0400

    feat(UniversalMessaging): Add log messages

--- src/main/kotlin/tech/ixor/routes/controller/universal/UniversalMessaging.kt
+++ src/main/kotlin/tech/ixor/routes/controller/universal/UniversalMessaging.kt
@@ -5,18 +5,23 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.slf4j.LoggerFactory
 import tech.ixor.I18N
 import tech.ixor.entity.*
 import tech.ixor.utils.UniversalMessagingUtil
 
 fun Route.universalMessaging() {
     val authKey = ConfigEntity().loadConfig().authKey
+    val logger = LoggerFactory.getLogger(javaClass)
 
     post("/send_message") {
+        logger.info(I18N.logging_universalMessaging_sendMessageRequestReceived())
+
         val request = call.receive<UniversalSendMessageRequest>()
         val response = UniversalMessagingResponse(0, mutableListOf<HTTPResponse>())
 
         if (authKey != request.authKey) {
+            logger.warn(I18N.logging_authkeyReceivedInvalid())
             response.responseCount = 1
             response.responseList.add(
                 HTTPResponse(HttpStatusCode.Forbidden.value, I18N.clientAuthkeyInvalid())
@@ -30,6 +35,7 @@ fun Route.universalMessaging() {
         val sender = request.sender
         val message = request.message
         if (senderID == null || source == null || sender == null || message == null) {
+            logger.warn(I18N.logging_universalMessaging_sendMessageRequestParamsMissing())
             response.responseCount = 1
             response.responseList.add(
                 HTTPResponse(HttpStatusCode.BadRequest.value, I18N.senderIDSourceSenderOrMessageIsNull())
@@ -37,6 +43,7 @@ fun Route.universalMessaging() {
             call.respond(response)
             return@post
         }
+        logger.info(I18N.logging_universalMessaging_sendMessageRequestParams(senderID, source, sender, message))
 
         call.respond(UniversalMessagingUtil.sendMessage(senderID, source, sender, message, emptyList()))
 
@@ -45,10 +52,13 @@ fun Route.universalMessaging() {
     }
 
     post("/broadcast") {
+        logger.info(I18N.logging_universalMessaging_broadcastRequestReceived())
+
         val request = call.receive<UniversalBroadcastRequest>()
         val response = UniversalMessagingResponse(0, mutableListOf<HTTPResponse>())
 
         if (authKey != request.authKey) {
+            logger.warn(I18N.logging_authkeyReceivedInvalid())
             response.responseCount = 1
             response.responseList.add(
                 HTTPResponse(HttpStatusCode.Forbidden.value, I18N.clientAuthkeyInvalid())
@@ -59,6 +69,7 @@ fun Route.universalMessaging() {
 
         val message = request.message
         if (message == null) {
+            logger.warn(I18N.logging_universalMessaging_broadcastRequestParamsMissing())
             response.responseCount = 1
             response.responseList.add(
                 HTTPResponse(HttpStatusCode.BadRequest.value, I18N.messageIsNull())
@@ -66,6 +77,7 @@ fun Route.universalMessaging() {
             call.respond(response)
             return@post
         }
+        logger.info(I18N.logging_universalMessaging_broadcastRequestParams(message))
 
         call.respond(UniversalMessagingUtil.broadcast(message, emptyList()))
 
