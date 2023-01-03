acd4225917207e404e9501ec5d0ea3e99b129b73
Cubik <cubik65536@cubik65536.top>
2023-01-02 21:11:45 -0500

    build: Use `refreshVersions` for managing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,6 @@ import java.io.ByteArrayOutputStream
 import java.io.FileInputStream
 import java.util.Properties
 
-val i18n4k_version: String by project
-val klaxon_version: String by project
-val krontab_version: String by project
-val hoplite_version: String by project
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 val versionPropertiesFile = "${projectDir}/project.properties"
 
 fun String.runCommand(currentWorkingDir: File = file("./")): String {
@@ -43,9 +35,9 @@ fun getStage(): String {
 
 plugins {
     application
-    kotlin("jvm") version "1.7.21"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("de.comahe.i18n4k") version "0.5.0"
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
+    id("de.comahe.i18n4k")
 }
 
 group = "tech.ixor"
@@ -102,29 +94,29 @@ repositories {
 dependencies {
     // Dependencies
     // i18n
-    implementation("de.comahe.i18n4k:i18n4k-core-jvm:$i18n4k_version")
+    implementation("de.comahe.i18n4k:i18n4k-core-jvm:_")
     // JSON Parser
-    implementation("com.beust:klaxon:$klaxon_version")
+    implementation("com.beust:klaxon:_")
     // Scheduled Jobs
-    implementation("dev.inmo:krontab:$krontab_version")
+    implementation("dev.inmo:krontab:_")
     // Config Loader
-    implementation("com.sksamuel.hoplite:hoplite-core:$hoplite_version")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:$hoplite_version")
+    implementation("com.sksamuel.hoplite:hoplite-core:_")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:_")
     // Ktor
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-html-builder-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-gson:$ktor_version")
-    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-client-cio-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:_")
+    implementation("io.ktor:ktor-server-netty-jvm:_")
+    implementation("io.ktor:ktor-server-html-builder-jvm:_")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:_")
+    implementation(Ktor.server.contentNegotiation)
+    implementation(Ktor.plugins.serialization.gson)
+    implementation("io.ktor:ktor-client-core-jvm:_")
+    implementation("io.ktor:ktor-client-cio-jvm:_")
     // Logback
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:_")
 
     // Test Dependencies
     // Ktor
     // Kotlin
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation(Kotlin.test.junit)
+    testImplementation("io.ktor:ktor-server-tests-jvm:_")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "RemoteMC-Core"
+
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
