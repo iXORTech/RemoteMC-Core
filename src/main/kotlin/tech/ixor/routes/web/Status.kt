e1a29f64a6f28e373f99994edb17d50c27c5b4e2
Cubik <cubik65536@cubik65536.top>
2023-03-07 21:51:22 -0500

    refactor(routes/web): Use extension function for html `page-wrapper` reuse

--- src/main/kotlin/tech/ixor/routes/web/Status.kt
+++ src/main/kotlin/tech/ixor/routes/web/Status.kt
@@ -20,136 +20,105 @@ fun Route.status() {
             qqBotAliveMonitor.forceUpdate()
             htmlPageHead("${I18N.websiteStatus}")
             body {
-                div {
-                    id = "page-wrapper"
-                    div("index-container") {
-                        div("content") {
-                            h2 { +"${I18N.websiteStatus}" }
+                pageWrapper("${I18N.websiteStatus}") {
+                    h2 {
+                        b("h1a") {
+                            +"${I18N.connections}"
+                        }
+                    }
 
-                            br { }
-
-                            h2 {
-                                b("h1a") {
-                                    +"${I18N.connections}"
-                                }
-                            }
-
-                            h3 {
-                                +"${I18N.minecraftServer}"
-                            }
-                            if (MinecraftServers.getOnlineServers().isNotEmpty()) {
-                                h4 {
-                                    +"${I18N.online} "
-                                }
-                            }
-                            ol {
-                                val onlineServers = MinecraftServers.getOnlineServers()
-                                for (minecraftServer in onlineServers) {
-                                    li {
-                                        if (minecraftServer.getCompatibilityStatus()) {
-                                            i {
-                                                attributes["style"] = "color: green;"
-                                                +"[Compatible] "
-                                            }
-                                        } else {
-                                            i {
-                                                attributes["style"] = "color: red;"
-                                                +"[Incompatible] "
-                                            }
-                                        }
-                                        b {
-                                            +minecraftServer.serverName
-                                        }
-                                        +" - ${minecraftServer.host} : ${minecraftServer.port}"
-                                        if (minecraftServer.default &&
-                                            MinecraftServers.getDefaultServer()?.equals(minecraftServer) == true
-                                        ) {
-                                            i {
-                                                +" ${I18N.default}"
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            if (MinecraftServers.getOfflineServers().isNotEmpty()) {
-                                h4 {
-                                    +"${I18N.offline}"
-                                }
-                            }
-                            ol {
-                                val offlineServers = MinecraftServers.getOfflineServers()
-                                for (minecraftServer in offlineServers) {
-                                    li {
-                                        b {
-                                            +minecraftServer.serverName
-                                        }
-                                        +" - ${minecraftServer.host} : ${minecraftServer.port}"
-                                    }
-                                }
-                            }
-
-                            h3 {
-                                +"${I18N.qqChatBot}"
-                            }
-                            val qqBot = QQBot.getBot()
-                            p {
-                                if (qqBot.getCompatibilityStatus()) {
+                    h3 {
+                        +"${I18N.minecraftServer}"
+                    }
+                    if (MinecraftServers.getOnlineServers().isNotEmpty()) {
+                        h4 {
+                            +"${I18N.online} "
+                        }
+                    }
+                    ol {
+                        val onlineServers = MinecraftServers.getOnlineServers()
+                        for (minecraftServer in onlineServers) {
+                            li {
+                                if (minecraftServer.getCompatibilityStatus()) {
                                     i {
                                         attributes["style"] = "color: green;"
                                         +"[Compatible] "
                                     }
                                 } else {
-                                    if (qqBot.checkOnlineStatus()) {
-                                        i {
-                                            attributes["style"] = "color: red;"
-                                            +"[Incompatible] "
-                                        }
+                                    i {
+                                        attributes["style"] = "color: red;"
+                                        +"[Incompatible] "
                                     }
                                 }
-                                +"${qqBot.host} : ${qqBot.port} - "
                                 b {
-                                    if (qqBot.checkOnlineStatus() == true) {
-                                        +"${I18N.isOnline}"
-                                    } else {
-                                        +"${I18N.isOffline}"
+                                    +minecraftServer.serverName
+                                }
+                                +" - ${minecraftServer.host} : ${minecraftServer.port}"
+                                if (minecraftServer.default &&
+                                    MinecraftServers.getDefaultServer()?.equals(minecraftServer) == true
+                                ) {
+                                    i {
+                                        +" ${I18N.default}"
                                     }
                                 }
                             }
-                            h4 {
-                                +"${I18N.qqGroups}"
+                        }
+                    }
+                    if (MinecraftServers.getOfflineServers().isNotEmpty()) {
+                        h4 {
+                            +"${I18N.offline}"
+                        }
+                    }
+                    ol {
+                        val offlineServers = MinecraftServers.getOfflineServers()
+                        for (minecraftServer in offlineServers) {
+                            li {
+                                b {
+                                    +minecraftServer.serverName
+                                }
+                                +" - ${minecraftServer.host} : ${minecraftServer.port}"
                             }
-                            ol {
-                                val qqGroups = qqBot.getQQGroups()
-                                for (qqGroup in qqGroups) {
-                                    li {
-                                        b {
-                                            +qqGroup.groupName
-                                        }
-                                        +" - ${qqGroup.groupCode}"
-                                    }
+                        }
+                    }
+
+                    h3 {
+                        +"${I18N.qqChatBot}"
+                    }
+                    val qqBot = QQBot.getBot()
+                    p {
+                        if (qqBot.getCompatibilityStatus()) {
+                            i {
+                                attributes["style"] = "color: green;"
+                                +"[Compatible] "
+                            }
+                        } else {
+                            if (qqBot.checkOnlineStatus()) {
+                                i {
+                                    attributes["style"] = "color: red;"
+                                    +"[Incompatible] "
                                 }
                             }
-
-                            br {}
-
-                            footer {
-                                hr {}
-                                a(href = "https://github.com/iXORTech/RemoteMC-Core/issues") {
-                                    +"${I18N.reportBug}"
+                        }
+                        +"${qqBot.host} : ${qqBot.port} - "
+                        b {
+                            if (qqBot.checkOnlineStatus()) {
+                                +"${I18N.isOnline}"
+                            } else {
+                                +"${I18N.isOffline}"
+                            }
+                        }
+                    }
+                    h4 {
+                        +"${I18N.qqGroups}"
+                    }
+                    ol {
+                        val qqGroups = qqBot.getQQGroups()
+                        for (qqGroup in qqGroups) {
+                            li {
+                                b {
+                                    +qqGroup.groupName
                                 }
-                                hr {}
-                                a(href = "https://github.com/iXORTech") {
-                                    i { attributes["data-feather"] = "github" }
-                                }
-                                +" | ${I18N.poweredBy} "
-                                a(href = "https://ixor.tech") { +"iXOR Technology" }
-                                +" ${I18N.withLove}"
-                                br {}
-                                +"${I18N.htmlThemeDesigned0}"
-                                a(href = "https://github.com/athul/archie") { +"Archie Theme" }
-                                +"${I18N.htmlThemeDesigned1}"
-                                a(href = "https://github.com/KevinZonda") { +"@KevinZonda" }
-                                +"${I18N.htmlThemeDesigned2}"
+                                +" - ${qqGroup.groupCode}"
                             }
                         }
                     }
